fd820b57d1c4690447b7f90277da05e1cd0057fa
Ahmed Lotfy <ahmed.lotfy@badrit.com>
2016-10-20 04:25:00 +0200

    Create Two empty tabs

---
+++ MokhtaserAlNaseha/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MokhtaserAlNaseha
+//
+//  Created by Ahmed Lotfy on 10/20/16.
+//  Copyright © 2016 Ahmed Lotfy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
